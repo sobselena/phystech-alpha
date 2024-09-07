f37182483d7d7ec5261db26d7a50b5798fab932f
Jackal Tabaqui <94977621+sobselena@users.noreply.github.com>
2024-09-07 15:58:34 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper(".swiper-container", { 
+  direction: "horizontal",
+  effect: "fade",
+  speed: 1000, 
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
